6e55c01e8f4cc8f28c86fb5a24cb6c360b9a1d6b
Jabrawl <jamesstibral@gmail.com>
2022-10-31 14:23:31 -0500

    James' changes

--- python.py
+++ python.py
@@ -1,1 +1,9 @@
-repo-week-1
+repo_week_1 = "these are my changes"
+
+
+more_changes = 2
+even_more_changes = 6
+final_changes = more_changes + even_more_changes
+
+print(final_changes)
+print(repo_week_1)
